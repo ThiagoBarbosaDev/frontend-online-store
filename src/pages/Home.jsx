ddca2ab3b00dac7a8fb1efdba641b0de0afb5ccf
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-07-07 18:50:44 -0300

    feat: req04 done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
+import Button from './Button';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const categories = await getCategories();
+    this.setState({ categories });
+  };
+
+  renderCategories = () => {
+    const { categories } = this.state;
+    return categories.map((data) => (
+      <Button
+        onClick={ () => console.log('xablau') }
+        type="button"
+        data-testid="category"
+        key={ data.id }
+      >
+        {data.name}
+      </Button>
+    ));
+  };
+
   render() {
     return (
       <div>
-        <Link
-          to="/cart"
-          data-testid="shopping-cart-button"
-        >
+        <Link to="/cart" data-testid="shopping-cart-button">
           Cart
         </Link>
+        <div>{this.renderCategories()}</div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
