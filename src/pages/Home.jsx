726978d4b446e913a5ea670360476e71b983e035
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-07-09 13:33:37 -0300

    comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,13 +47,11 @@ class Home extends React.Component {
     const { inputSearch } = this.state;
     const queryData = await getProductsFromCategoryAndQuery('', inputSearch);
     this.setState({ queryData: queryData.results });
-    // console.log(queryData);
   }
 
   renderCards = () => {
     const { queryData } = this.state;
     const data = queryData;
-    // console.log(queryData.results);
     return data.map((item) => <CardItem key={ item.id } data={ item } />);
   }
 
@@ -61,12 +59,10 @@ class Home extends React.Component {
     this.setState({
       inputSearch: e.target.value,
     });
-    // console.log(e.target.value);
   }
 
   render() {
     const { inputSearch, queryData } = this.state;
-    // console.log(queryData.length);
     return (
       <div>
         <input
