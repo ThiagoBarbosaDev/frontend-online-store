558932fbcb6f755873c6a38272137a1d09d68372
Thiago-NC <thiagonc.84@gmail.com>
2022-07-10 12:39:33 -0300

    feat: req10 done

--- src/Components/ShoppingCartItem.jsx
+++ src/Components/ShoppingCartItem.jsx
@@ -2,12 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ShoppingCartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quantity: 1,
+    };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.quantity;
+  }
+
+  increaseItem = () => {
+    this.setState((prevState) => ({ quantity: prevState.quantity + 1 }));
+  }
+
+  decreaseItem = () => {
+    this.setState((prevState) => ({ quantity: prevState.quantity - 1 }));
+  }
+
   render() {
     const { data } = this.props;
+    const { quantity } = this.state;
     return (
       <li>
         <p data-testid="shopping-cart-product-name">{ data.title }</p>
-        <p data-testid="shopping-cart-product-quantity">1</p>
+        <button
+          data-testid="product-decrease-quantity"
+          type="button"
+          onClick={ this.decreaseItem }
+        >
+          -
+        </button>
+        <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
+        <button
+          data-testid="product-increase-quantity"
+          type="button"
+          onClick={ this.increaseItem }
+        >
+          +
+        </button>
         <p>{ data.price }</p>
       </li>
     );
